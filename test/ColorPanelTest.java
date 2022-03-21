c1352bab8ace8e74ad649831cc3e3799c6b07533
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 21:01:40 +0000

    Create ColorPanelTest.java

---
+++ test/ColorPanelTest.java
@@ -0,0 +1,16 @@
+package colorpanel.test;
+
+import colorpanel.base.ColorPanel;
+
+public class ColorPanelTest {
+
+    public static void main(String[] args) {
+
+        ColorPanel newColorPanel = new ColorPanel(); 
+      
+    
+    }
+    
+
+
+}
